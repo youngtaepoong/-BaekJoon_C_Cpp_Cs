11d038b3c0f45c5b1aa26b47717eb196ff3948a4
youngtaepoong <76558186+youngtaepoong@users.noreply.github.com>
2022-01-04 21:02:47 +0900

    Add files via upload

---
+++ We♡kriii.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Comment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한친구 대한육군");
+            Console.WriteLine("강한친구 대한육군");
+        }
+    }
+}
