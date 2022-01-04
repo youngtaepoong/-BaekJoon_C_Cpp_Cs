94510e8c810214dc5fa279e41dd11177e3c2f876
youngtaepoong <76558186+youngtaepoong@users.noreply.github.com>
2022-01-04 21:23:16 +0900

    Add files via upload

---
+++ catPrint.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace 입출력고양이
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // c#에서 \를 출력하기 위해서는 \\ 이렇게 두번 적어주어야한다.
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
