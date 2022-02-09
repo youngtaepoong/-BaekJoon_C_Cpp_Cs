11c5f804848cc8b73bba2c915c42252df7e4780b
youngtaepoong <76558186+youngtaepoong@users.noreply.github.com>
2022-02-09 22:31:49 +0900

    Add files via upload

--- Math1/sugarDelivery.c
+++ Math1/sugarDelivery.c
@@ -4,33 +4,22 @@ int main(void)
 {
 	int n, cnt = 0;
 	scanf("%d", &n);
-						 //18 4  6
-	printf("%d\n", n/3); // 6 1 2
-	printf("%d\n", n/5); // 3 0 1
-	printf("%d\n", n%3); // 0 1 0
-	printf("%d\n", n%5); // 3 4 1
 	
-	cnt = (n/5 > n/3) ? n/3 : n/5;
-	
-	
-	
-	
-	if (n/5 > 0)
+	while(1)
 	{
-		cnt = n/5;
-		n-= cnt*5;
-		if (n > 0)
+		if (n % 5 == 0)
 		{
-			cnt+=(n/3);
+			cnt += n / 5;
+			break;
 		}
+		
+		n -= 3;
+		cnt++;
+		if (n <= 0 )
+			break;
 	}
-	else if (n/3 > 0)
-	{
-		cnt = n/3;
-		n-= cnt*3;
-	}
-	else
-		cnt = -1;
 	
-	printf("%d\n", cnt);
+	
+	if (n < 0) printf("-1\n");
+	else printf("%d\n", cnt);
 }
